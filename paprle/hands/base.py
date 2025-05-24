d7d668618ff657aa8b3e0fc3590559af15937658
obin <obin.kwon000@gmail.com>
2025-05-24 02:35:23 -0500

    basic implementation

---
+++ paprle/hands/base.py
@@ -0,0 +1,10 @@
+
+class BaseHand:
+    def __init__(self, *args, **kwargs):
+        return
+
+    def solve(self, command=None):
+        if command is not None: return command
+
+    def reset(self):
+        return
